3f79ce0be6e503e6d23d40c3e7fad5bd65b70ba9
Jac0b0995 <andres9montero9@gmail.com>
2023-10-30 17:42:58 -0500

    Activity list added with checkmark

--- tasks/models.py
+++ tasks/models.py
@@ -12,3 +12,13 @@ class Task(models.Model):
 
     class Meta:
         app_label = 'tasks'
+        
+class Activity(models.Model):
+    activity = models.CharField(max_length=200)
+    date = models.DateField(null=True, blank=True)
+    start_time = models.TimeField(null=True, blank=True)
+    end_time = models.TimeField(null=True, blank=True)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, null=True)  # Make 'task' nullable
+
+    def __str__(self):
+        return self.activity
--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import redirect, render
-from .models import Task
+from .models import Task, Activity
+from django.http import HttpResponse
+
 
 # Create your views here.
 
@@ -32,3 +34,20 @@ def task_detail(request, task_id):
 
 def hello_world(request):
     return render(request, 'tasks/hello_world.html')
+
+
+def task_detail(request, task_id):
+    task = Task.objects.get(id=task_id)
+
+    if request.method == 'POST':
+        activity_name = request.POST.get('activity')
+        date = request.POST.get('date')
+        start_time = request.POST.get('start_time')
+        end_time = request.POST.get('end_time')
+
+        activity = Activity(activity=activity_name, date=date, start_time=start_time, end_time=end_time, task=task)
+        activity.save()
+
+    activities = Activity.objects.filter(task_id=task.id)  # Use 'task_id' field to filter activities
+
+    return render(request, 'tasks/task_detail.html', {'task': task, 'activities': activities})
