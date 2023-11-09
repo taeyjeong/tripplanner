68e9e6e6ad28a7aa78c16d357ed786b0d8882dbd
lucaspark-99 <65432861+lucaspark-99@users.noreply.github.com>
2023-11-09 19:42:22 -0300

    Add files via upload
    merged andres' branch into lucas' branch

---
+++ app/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateField(null=True, blank=True)
+    start_time = models.TimeField(null=True, blank=True)
+    end_time = models.TimeField(null=True, blank=True)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        app_label = 'registration'
+
+class Activity(models.Model):
+    activity = models.CharField(max_length=200)
+    date = models.DateField(null=True, blank=True)
+    start_time = models.TimeField(null=True, blank=True)
+    end_time = models.TimeField(null=True, blank=True)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, null=True)  # Make 'task' nullable
+
+    def __str__(self):
+        return self.activity
+    
+    class Meta:
+        app_label = 'registration'
---
+++ app/views.py
@@ -0,0 +1,93 @@
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from .models import Task, Activity
+
+
+# Create your views here.
+@login_required(login_url = 'login')
+def HomePage(request):
+    return render(request, 'home.html')
+
+def SignupPage(request):
+    if request.method=='POST':
+        username=request.POST.get('username')
+        email=request.POST.get('email')
+        password1=request.POST.get('password1')
+        password2=request.POST.get('password2')
+
+        if password1 != password2:
+            return HttpResponse("Your passwords dont match.")
+        else:
+            my_user= User.objects.create_user(username, email, password1)
+            my_user.save()
+            return redirect('login')
+        
+
+    return render(request, 'signup.html')
+
+def LoginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password1 = request.POST.get('password')
+        user = authenticate(request, username=username, password=password1)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            return HttpResponse('Username or Password is incorrect')
+    
+    return render(request, 'login.html')
+
+def LogoutPage(request):
+    logout(request)
+    return redirect('login')
+
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'task_list.html', {'tasks': tasks})
+
+
+def create_task(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        date = request.POST.get('date')
+        start_time = request.POST.get('start_time')
+        end_time = request.POST.get('end_time')
+
+        # Create a new task with the provided data
+        task = Task(title=title, date=date, start_time=start_time, end_time=end_time)
+        task.save()
+
+    return redirect('task_list')
+
+def delete_task(request, task_id):
+    if request.method == 'POST':
+        task = Task.objects.get(id=task_id)
+        task.delete()
+    return redirect('task_list')
+
+def task_detail(request, task_id):
+    task = Task.objects.get(pk=task_id)
+    return render(request, 'task_detail.html', {'task': task})
+
+def hello_world(request):
+    return render(request, 'hello_world.html')
+
+
+def task_detail(request, task_id):
+    task = Task.objects.get(id=task_id)
+
+    if request.method == 'POST':
+        activity_name = request.POST.get('activity')
+        date = request.POST.get('date')
+        start_time = request.POST.get('start_time')
+        end_time = request.POST.get('end_time')
+
+        activity = Activity(activity=activity_name, date=date, start_time=start_time, end_time=end_time, task=task)
+        activity.save()
+
+    activities = Activity.objects.filter(task_id=task.id)  # Use 'task_id' field to filter activities
+
+    return render(request, 'task_detail.html', {'task': task, 'activities': activities})
