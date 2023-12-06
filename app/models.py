99b09c731176965ee73207e2d030b6d63008357e
lucaspark-99 <lucaspark0@gmail.com>
2023-12-06 19:54:53 -0300

    fixed some spelling on templates

--- app/models.py
+++ app/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-
-# Create your models here.
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     title = models.CharField(max_length=200)
