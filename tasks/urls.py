a13a857ca61ed42fe3a2e2b79469eb791e108e45
Jac0b0995 <andres9montero9@gmail.com>
2023-12-13 17:31:06 -0500

    API fully tested, Add activity tested, urls organized, pending more css adjustments

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,11 +1,14 @@
+# urls.py
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.task_list, name='task_list'),
+    path('', views.hello_world, name='hello_world'),
+    path('task_list/', views.task_list, name='task_list'),
     path('create/', views.create_task, name='create_task'),
     path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
-    path('hello-world/', views.hello_world, name='hello_world'),
-    path('task/<int:task_id>/', views.task_detail, name='task_detail'),
-    path('task_detailr/', views.flight_tracker, name='flight_tracker'),
+    path('task_detail/<int:task_id>/', views.task_detail, name='task_detail'),
+    path('add_activity/<int:task_id>/', views.add_activity, name='add_activity'),
+
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,5 @@
-# tasks/views.py
+# views.py
+
 from django.shortcuts import render, redirect
 from django.conf import settings
 from .models import Task, Activity, Flight
@@ -10,30 +11,27 @@ def task_list(request):
 
 def create_task(request):
     if request.method == 'POST':
-        title = request.POST.get('title')
-        date = request.POST.get('date')
-        start_time = request.POST.get('start_time')
-        end_time = request.POST.get('end_time')
-
-        task = Task(title=title, date=date, start_time=start_time, end_time=end_time)
-        task.save()
-
+        Task.objects.create(
+            title=request.POST.get('title'),
+            date=request.POST.get('date'),
+            start_time=request.POST.get('start_time'),
+            end_time=request.POST.get('end_time')
+        )
     return redirect('task_list')
 
 def delete_task(request, task_id):
     if request.method == 'POST':
-        task = Task.objects.get(id=task_id)
-        task.delete()
+        Task.objects.filter(id=task_id).delete()
     return redirect('task_list')
 
 def hello_world(request):
+    if request.method == 'POST':
+        return redirect('task_list')
     return render(request, 'hello_world.html')
 
 def task_detail(request, task_id):
     task = Task.objects.get(id=task_id)
     activities = Activity.objects.filter(task_id=task.id)
-
-    form = request.POST.get('flight_number')
 
     if request.method == 'POST':
         flight_number = request.POST.get('flight_number')
@@ -56,38 +54,17 @@ def task_detail(request, task_id):
             return render(request, 'task_detail.html', {'task': task, 'activities': activities, 'flight': flight})
         else:
             error_message = 'Flight not found. Please enter a valid flight number.'
-            return render(request, 'task_detail.html', {'task': task, 'activities': activities, 'form': form, 'error_message': error_message})
+            return render(request, 'task_detail.html', {'task': task, 'activities': activities, 'error_message': error_message})
 
-    return render(request, 'task_detail.html', {'task': task, 'activities': activities, 'form': form})
+    return render(request, 'task_detail.html', {'task': task, 'activities': activities})
 
-def flight_tracker(request, task_id):
-    task = Task.objects.get(id=task_id)
-    activities = Activity.objects.filter(task_id=task.id)
-    form = FlightForm(request.POST or None)
-
+def add_activity(request, task_id):
     if request.method == 'POST':
-        form = FlightForm(request.POST)
-        if form.is_valid():
-            flight_number = form.cleaned_data.get('flight_number')
-            aviationstack_api_key = settings.AVIATIONSTACK_API_KEY
-            api_url = f'http://api.aviationstack.com/v1/flights?access_key={aviationstack_api_key}&flight_iata={flight_number}'
-
-            response = requests.get(api_url)
-            data = response.json()
-
-            if data['pagination']['total'] > 0:
-                flight_data = data['data'][0]
-
-                flight = Flight.objects.create(
-                    flight_number=flight_data['flight']['iata'],
-                    airline=flight_data['airline']['name'],
-                    departure_airport=flight_data['departure']['iata'],
-                    arrival_airport=flight_data['arrival']['iata']
-                )
-
-                return render(request, 'task_detail.html', {'task': task, 'activities': activities, 'flight': flight})
-            else:
-                error_message = 'Flight not found. Please enter a valid flight number.'
-                return render(request, 'task_detail.html', {'task': task, 'activities': activities, 'form': form, 'error_message': error_message})
-
-    return render(request, 'task_detail.html', {'task': task, 'activities': activities, 'form': form})
+        Activity.objects.create(
+            task_id=task_id,
+            activity=request.POST.get('activity'),
+            date=request.POST.get('date'),
+            start_time=request.POST.get('start_time'),
+            end_time=request.POST.get('end_time')
+        )
+    return redirect('task_detail', task_id=task_id)
